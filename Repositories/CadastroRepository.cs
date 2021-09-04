459ea3499b0fb5bb1ca7b5baa18543e3b552d743
Eliseu Oliveira <eliseu.dev@outlook.com>
2021-09-03 23:54:56 -0300

    Carrossel de Produtos Criado

---
+++ Repositories/CadastroRepository.cs
@@ -0,0 +1,15 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class CadastroRepository : BaseRepository<ItemPedido>
+    {
+        public CadastroRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+}
