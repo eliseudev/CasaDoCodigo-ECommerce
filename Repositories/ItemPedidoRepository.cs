9c3434f4fac62bc84beee0461317087412975296
Eliseu Oliveira <eliseu.dev@outlook.com>
2021-09-07 22:36:03 -0300

    Implementação Quantidade

--- Repositories/ItemPedidoRepository.cs
+++ Repositories/ItemPedidoRepository.cs
@@ -8,12 +8,23 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDB = dbSet.Where(p => p.Id == itemPedido.Id).SingleOrDefault();
+
+            if(itemPedidoDB != null)
+            {
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
+                contexto.SaveChanges();
+            }
         }
     }
 }
